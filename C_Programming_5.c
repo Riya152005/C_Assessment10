7a7eb99c561073f6e0823d57b712c686a6265062
Riya152005 <riyarathna1507@gmail.com>
2026-06-23 21:46:18 +0530

    Create C_Programming_5.c

---
+++ C_Programming_5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    char str[20];
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    sprintf(str, "%d", num);
+    printf("String = %s", str);
+}
